a22aa7d008a306602643744cb423172504a81df2
rachelrobbinsmayhill <rachel.robbinsmayhill@gmail.com>
2022-04-04 16:57:23 -0500

    Updated functions

--- wrangle_mall.py
+++ wrangle_mall.py
@@ -1,5 +1,15 @@
 from imports import *
 
+'''
+1. Acquire data from mall_customers.customers in mysql database.
+2. Missing values
+3. One-hot-encoding (pd.get_dummies)
+4. Split the data into train, validate, and split
+5. Scaling
+Add, commit, and push all of your work.
+'''
+
+
 
 def get_mall_data(use_cache=True):
     filename = 'mall_customers.csv'
@@ -23,7 +33,6 @@ def get_mall_data(use_cache=True):
     return df
 
 
-
 def summary_info(df): 
     # Summarize data (shape, info, summary stats, dtypes, shape)
     print('--- Shape: {}'.format(df.shape))
@@ -32,9 +41,12 @@ def summary_info(df):
     print('--- Info')
     return df.info()
 
+def one_hot_encode(df):
+    df['is_female'] = df.gender == 'Female'
+    df = df.drop(columns='gender')
+    return df
 
-
-def data_split(df, target):
+def split(df):
     '''
     This function splits a dataframe into train, validate, and test in order to 
     explore the data and to create and validate models. 
@@ -45,12 +57,42 @@ def data_split(df, target):
     The function returns, train, validate and test dataframes. 
     '''
     
-    train, test = train_test_split(df, test_size = .2, random_state=123)  
-    train, validate = train_test_split(train, test_size=.3, random_state=123)
+    train_and_validate, test = train_test_split(df, random_state=123, test_size=.15)
+    train, validate = train_test_split(train_and_validate, random_state=123, test_size=.2)
 
-
-    print(f'train -> {train.shape}')
-    print(f'validate -> {validate.shape}')
-    print(f'test -> {test.shape}')
+    print('Train: %d rows, %d cols' % train.shape)
+    print('Validate: %d rows, %d cols' % validate.shape)
+    print('Test: %d rows, %d cols' % test.shape)
     
     return train, validate, test
+
+
+
+def scale(train, validate, test):
+    columns_to_scale = ['age', 'spending_score', 'annual_income']
+    train_scaled = train.copy()
+    validate_scaled = validate.copy()
+    test_scaled = test.copy()
+
+    scaler = MinMaxScaler()
+    scaler.fit(train[columns_to_scale])
+
+    train_scaled[columns_to_scale] = scaler.transform(train[columns_to_scale])
+    validate_scaled[columns_to_scale] = scaler.transform(validate[columns_to_scale])
+    test_scaled[columns_to_scale] = scaler.transform(test[columns_to_scale])
+
+    return train_scaled, validate_scaled, test_scaled
+
+def get_exploration_data():
+    df = acquire()
+    train, validate, test = split(df)
+    return train
+
+def get_modeling_data(scale_data=False):
+    df = acquire()
+    df = one_hot_encode(df)
+    train, validate, test = split(df)
+    if scale_data:
+        return scale(train, validate, test)
+    else:
+        return train, validate, test
